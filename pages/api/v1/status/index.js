731b985860d73d24a3bf8a52db694ad7ae7d2194
GilmarFRJR <gilmarfrj@gmail.com>
2024-05-31 09:11:07 -0300

    add `database.js`and `.env` files

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
+  res.status(200).json({ teste: "ok" });
+}
+
+export default status;
